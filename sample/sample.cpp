6e85175d55d84daef90da31553085749186ab636
MistEO <mistereo@hotmail.com>
2021-02-07 00:22:13 +0800

    update sample

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 #include <unistd.h>
-#include <float.h>
 
 #include "physics.h"
 
@@ -30,23 +30,38 @@ void interstellar_and_planet()
     space.emplace_object(month, Coordinate(384403.9e3, 0));
 
     printf("\033[?25l\033[2J");
-    Coordinate pre_earth(0, 0);
-    Coordinate pre_month(0, 0);
+    int earth_pre_x = 0, earth_pre_y = 0, month_pre_x = 0, month_pre_y = 0;
 
     space.time().timeflow() = 1000000;
     space.time().start();
 
     while (true)
     {
-        auto earth_coor = Coordinate(20, 20);
-        auto month_coor = 2 * (space.get_coor(month) - space.get_coor(earth)) / 1e8 + Coordinate(20, 20);
+        auto &&[earth_doublex, earth_doubley] = Coordinate(20, 20);
+        auto &&[month_doublex, month_doubley] = 2 * (space.get_coor(month) - space.get_coor(earth)) / 1e8 + Coordinate(20, 20);
+
+        int earth_x = static_cast<int>(std::round(earth_doublex));
+        int earth_y = static_cast<int>(std::round(earth_doubley / 2));
+        int month_x = static_cast<int>(std::round(month_doublex));
+        int month_y = static_cast<int>(std::round(month_doubley / 2));
+
+        if (earth_x == earth_pre_x && earth_y == earth_pre_y &&
+            month_x == month_pre_x && month_pre_y == earth_pre_y)
+        {
+            usleep(10);
+            continue;
+        }
+
         printf("\033[%d;%dH \033[%d;%dHO\033[%d;%dH \033[%d;%dHO\n",
-               int(pre_earth.second / 2), int(pre_earth.first), int(earth_coor.second / 2), int(earth_coor.first),
-               int(pre_month.second / 2), int(pre_month.first), int(month_coor.second / 2), int(month_coor.first));
-        //printf("\033[%d;%dH \033[%d;%dHO\n", int(pre_month.second), int(pre_earth.first), int(month_coor.second), int(month_coor.first));
-        pre_earth = earth_coor;
-        pre_month = month_coor;
-        usleep(100);
+               earth_pre_y, earth_pre_x, earth_y, earth_x,
+               month_pre_y, month_pre_x, month_y, month_x);
+
+        earth_pre_x = earth_x;
+        earth_pre_y = earth_y;
+        month_pre_x = month_x;
+        month_pre_y = month_y;
+
+        usleep(10);
     }
 }
 
@@ -62,15 +77,15 @@ void world_and_ball()
     world.emplace_object(ball, Coordinate(0, 10));
 
     printf("\033[?25l\033[2J");
-    int pre_x = 1, pre_y = 1;
+    int pre_x = 0, pre_y = 0;
 
     world.time().start();
 
     while (true)
     {
-        auto coor = world.get_coor(ball) + Coordinate(1, 1);
-        int x = (coor.first > 0.0) ? (coor.first + 0.5) : (coor.first - 0.5);
-        int y = (coor.second > 0.0) ? (coor.second + 0.5) : (coor.second - 0.5);
+        auto &&[doublex, doubley] = world.get_coor(ball) + Coordinate(1, 1);
+        int x = static_cast<int>(std::round(doublex));
+        int y = static_cast<int>(std::round(doubley));
         y = 11 - y;
         if (pre_x == x && pre_y == y)
         {
