12f1839136bfd9c11dc4697176f4b3d92e3669de
MistEO <mistereo@hotmail.com>
2021-02-08 20:22:50 +0800

    update sample

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <cstdarg>
 #include <unistd.h>
 
 #include "physics.h"
@@ -9,6 +10,7 @@ using namespace meophys;
 
 void world_and_ball();
 void interstellar_and_planet();
+void print_loop(const Space *space, std::vector<std::shared_ptr<Object>> &&objects, double scale = 1.0);
 
 int main()
 {
@@ -24,51 +26,16 @@ void interstellar_and_planet()
     //auto sun = std::make_shared<Object>("Sun", 1.989e30);
     auto earth = std::make_shared<Object>("Earth", 5.965e24);
     auto month = std::make_shared<Object>("Month", 7.349e22);
-    month->set_velocity(Velocity(0, 1.023e3));
+    month->set_velocity(0, 1.023e3);
 
     //space.emplace_object(sun, Coordinate(0, 0));
     space.emplace_object(earth, Coordinate(0, 0));
     space.emplace_object(month, Coordinate(384403.9e3, 0));
 
-    printf("\033[?25l\033[2J");
-    int earth_pre_x = 0, earth_pre_y = 0, month_pre_x = 0, month_pre_y = 0;
-
     space.time().timeflow() = 1000000;
     space.time().start();
 
-    while (true)
-    {
-        auto &&[earth_doublex, earth_doubley] = Coordinate(20, 20);
-        auto &&[month_doublex, month_doubley] = 2 * (space.get_coor(month) - space.get_coor(earth)) / 1e8 + Coordinate(20, 20);
-
-        int earth_x = static_cast<int>(std::round(earth_doublex));
-        int earth_y = static_cast<int>(std::round(earth_doubley / 2));
-        int month_x = static_cast<int>(std::round(month_doublex));
-        int month_y = static_cast<int>(std::round(month_doubley / 2));
-
-        auto elapsed = space.time().elapsed<std::ratio<3600 * 24>>().count();
-
-        if (earth_x == earth_pre_x && earth_y == earth_pre_y &&
-            month_x == month_pre_x && month_pre_y == earth_pre_y)
-        {
-            usleep(10);
-            continue;
-        }
-
-        printf(
-            "\033[0;0Helapsed: %ld days\033[%d;%dH \033[%d;%dHO\033[%d;%dH \033[%d;%dHO\n",
-            elapsed,
-            earth_pre_y,
-            earth_pre_x, earth_y, earth_x,
-            month_pre_y, month_pre_x, month_y, month_x);
-
-        earth_pre_x = earth_x;
-        earth_pre_y = earth_y;
-        month_pre_x = month_x;
-        month_pre_y = month_y;
-
-        usleep(10);
-    }
+    print_loop(&space, {earth, month}, 1e-8);
 }
 
 void world_and_ball()
@@ -80,29 +47,49 @@ void world_and_ball()
     ball->elasticity() = 0.9;
     ball->friction() = 0.5;
     ball->exert_force(Force(0, -9.8));
-    ball->set_velocity(Velocity(5, 0));
+    ball->set_velocity(5, 0);
+
+    auto ball_2 = std::make_shared<Object>("Ball2", 1);
+    ball_2->elasticity() = 0.9;
+    ball_2->friction() = 0.5;
+    ball_2->exert_force(Force(0, -9.8));
+    ball_2->set_velocity(-5, 0);
 
     world.emplace_object(ball, Coordinate(1, 10));
-
-    printf("\033[?25l\033[2J");
-    int pre_x = 0, pre_y = 0;
+    world.emplace_object(ball_2, Coordinate(20, 10));
 
     world.time().start();
 
+    print_loop(&world, {ball, ball_2});
+}
+
+void print_loop(const Space *space, std::vector<std::shared_ptr<Object>> &&objects, double scale)
+{
+    printf("\033[?25l\033[2J");
+
+    std::vector<std::pair<int, int>> pre_coor(objects.size());
+
+    std::string print_str = "\033[%d;%dH \033[%d;%dHO";
+
     while (true)
     {
-        auto &&[doublex, doubley] = world.get_coor(ball) + Coordinate(1, 1);
-        int x = static_cast<int>(std::round(doublex));
-        int y = static_cast<int>(std::round(doubley));
-        y = 11 - y;
-        if (pre_x == x && pre_y == y)
+        for (size_t i = 0; i != objects.size(); ++i)
         {
-            usleep(10);
-            continue;
+            auto &&[doublex, doubley] = space->get_coor(objects[i]) * scale;
+            int x = static_cast<int>(std::round(doublex)) + 30;
+            int y = -static_cast<int>(std::round(doubley / 2.0)) + 10;
+
+            auto &&[pre_x, pre_y] = pre_coor[i];
+            if (pre_x == x && pre_y == y)
+            {
+                usleep(10);
+                continue;
+            }
+            printf("\033[%d;%dH \033[%d;%dHO\n", pre_y, pre_x, y, x);
+
+            pre_x = x;
+            pre_y = y;
         }
-        printf("\033[%d;%dH \033[%d;%dHO\n", pre_y, pre_x, y, x);
-        pre_x = x;
-        pre_y = y;
         usleep(10);
     }
 }
