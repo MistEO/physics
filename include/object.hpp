6fcc1a7a01d78c18390dcdda3268e6660cf6e1f8
MistEO <mistereo@hotmail.com>
2021-02-09 00:47:12 +0800

    add unique_id int the Object

--- include/object.hpp
+++ include/object.hpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <atomic>
+#include <mutex>
 #include "define.h"
 
 namespace meophys
@@ -10,14 +12,22 @@ namespace meophys
     class Object
     {
     public:
-        Object() = default;
+        Object()
+        {
+            std::unique_lock<std::mutex> lock(_id_mutex);
+            _unique_id = _ref_id++;
+        }
         Object(std::string name, long double mass) : _name(std::move(name)), _mass(mass)
         {
+            {
+                std::unique_lock<std::mutex> lock(_id_mutex);
+                _unique_id = _ref_id++;
+            }
             if (mass <= 0)
                 throw std::invalid_argument("mass must be positive");
         }
-        Object(const Object &obj) = default;
-        Object(Object &&obj) = default;
+        Object(const Object &rhs) { *this = rhs; }
+        Object(Object &&rhs) { *this = std::move(rhs); }
 
         virtual ~Object() = default;
 
@@ -30,21 +40,49 @@ namespace meophys
         virtual double &elasticity() noexcept { return _elasticity; }
         virtual const double &friction() const noexcept { return _friction; }
         virtual double &friction() noexcept { return _friction; }
+        virtual const int &unique_id() const noexcept { return _unique_id; }
 
-        Object &operator=(const Object &rhs) = default;
-        Object &operator=(Object &&rhs) = default;
+        Object &operator=(const Object &rhs)
+        {
+            {
+                std::unique_lock<std::mutex> lock(_id_mutex);
+                _unique_id = _ref_id++;
+            }
+            _name = rhs._name + "_copy";
+            _mass = rhs._mass;
+            _radius = rhs._radius;
+            _elasticity = rhs._elasticity;
+            _friction = rhs._friction;
+            return *this;
+        }
+        Object &operator=(Object &&rhs)
+        {
+            _unique_id = rhs._unique_id;
+            rhs._unique_id = 0;
+            _name = std::move(rhs._name);
+            _mass = std::move(rhs._mass);
+            _radius = std::move(rhs._radius);
+            _elasticity = std::move(rhs._elasticity);
+            _friction = std::move(rhs._friction);
+            return *this;
+        }
         const bool operator==(const Object &rhs) const
         {
-            return (_name == rhs._name) && (_mass == rhs._mass);
+            return (_unique_id == rhs._unique_id) && (_name == rhs._name);
         }
 
     protected:
+        std::mutex _id_mutex;
+        static std::atomic_int _ref_id;
+        int _unique_id;           // 唯一id，用于判断相等，计算hash值等
         std::string _name;        // 名字，无实际作用
         long double _mass = 1.0;  // 质量，单位kg
         double _radius = 1.0;     // 半径，单位m
         double _elasticity = 1.0; // 弹性系数
         double _friction = 0;     // 摩擦系数
     };
+
+    std::atomic_int Object::_ref_id = 0;
 
 } // namespace meophys
 
@@ -56,7 +94,7 @@ namespace std
     public:
         size_t operator()(const meophys::Object &rhs) const noexcept
         {
-            return (hash<std::string>()(rhs.name()) ^ hash<long double>()(rhs.mass()) << 1);
+            return ((hash<int>()(rhs.unique_id())) && hash<std::string>()(rhs.name()) << 1);
         }
     };
 } // namespace std
